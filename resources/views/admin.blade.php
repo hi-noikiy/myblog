1d558aac0750908c40a12b25bea2193d0bcd5d35
chankwen <418415881@qq.com>
2018-03-27 18:15:36 +0800

    fix

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
-    <link rel="stylesheet" href="/back/css/app.css">
+    <link rel="stylesheet" href="{{mix('/back/css/app.css', 'back')}}">
 </head>
 <body>
 <div id="app"></div>
-<script src="/back/js/manifest.js"></script>
-<script src="/back/js/vendor.js"></script>
-<script src="/back/js/app.js"></script>
+<script src="{{mix('/back/js/manifest.js', 'back')}}"></script>
+<script src="{{mix('/back/js/vendor.js', 'back')}}"></script>
+<script src="{{mix('/back/js/app.js', 'back')}}"></script>
 </body>
 </html>
