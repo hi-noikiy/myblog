1e1f889794303372f5d0bd4d9e25c163e565d93a
chankwen <418415881@qq.com>
2018-03-28 22:19:53 +0800

    fix webpack.mix结构

--- webpack.admin.js
+++ webpack.admin.js
@@ -1,16 +1,17 @@
 const mix = require('laravel-mix')
 
+if (mix.inProduction()) {
+  mix.version()
+}
+
 mix.webpackConfig({
   output: {
     publicPath: '/back/',
     chunkFilename: `js/[name].${mix.inProduction() ? '[chunkhash].' : ''}js`
   }
 })
-mix.setResourceRoot('/back/')
-mix.js('resources/admin/assets/js/app.js', 'public/back/js')
-  .sass('resources/admin/assets/sass/app.scss', 'public/back/css')
-  .extract(['vue', 'iview', 'vue-router', 'axios'])
-  .setPublicPath('public/back/')
-if (mix.inProduction()) {
-  mix.version()
-}
+mix.js('resources/admin/assets/js/app.js', 'public/back/js') // 打包后台js
+  .sass('resources/admin/assets/sass/app.scss', 'public/back/css') // 打包后台css
+  .extract(['vue', 'iview', 'vue-router', 'axios']) // 提取依赖库
+  .setResourceRoot('/back/') // 设置资源目录
+  .setPublicPath('public/back/') // 设置默认打包目录
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,15 @@
 const mix = require('laravel-mix')
 const { env } = require('minimist')(process.argv.slice(2))
+
 if (env && env.admin) {
   require(`${__dirname}/webpack.admin.js`)
   return
 }
 
-mix.js('resources/frontend/assets/js/app.js', 'public/js')
-  .sass('resources/frontend/assets/sass/app.scss', 'public/css')
-  .extract(['vue'])
 if (mix.inProduction()) {
   mix.version()
 }
+
+mix.js('resources/frontend/assets/js/app.js', 'public/js')
+  .sass('resources/frontend/assets/sass/app.scss', 'public/css')
+  .extract(['vue'])
