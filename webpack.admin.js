f260e837f91a8fa1dff15d4d0bbc8ab431d6ab19
chankwen <418415881@qq.com>
2018-03-29 20:18:11 +0800

    fix

--- webpack.admin.js
+++ webpack.admin.js
@@ -6,12 +6,13 @@ if (mix.inProduction()) {
 
 mix.webpackConfig({
   output: {
-    publicPath: '/back/',
-    chunkFilename: `js/[name].${mix.inProduction() ? '[chunkhash].' : ''}js`
+    publicPath: '/back/', // 设置默认打包目录
+    chunkFilename: `js/[name].${mix.inProduction() ? '[chunkhash].' : ''}js` // 路由懒加载的时候打包出来的js文件
   }
 })
+
 mix.js('resources/admin/assets/js/app.js', 'public/back/js') // 打包后台js
   .sass('resources/admin/assets/sass/app.scss', 'public/back/css') // 打包后台css
   .extract(['vue', 'iview', 'vue-router', 'axios']) // 提取依赖库
   .setResourceRoot('/back/') // 设置资源目录
-  .setPublicPath('public/back/') // 设置默认打包目录
+  .setPublicPath('public/back')
